b9f1d895eef0fa8b78a687d5660bce619150e95e
TheoCou <91134816+TheoCou@users.noreply.github.com>
2023-02-01 16:19:50 +0100

    modified:   page "connecté"

--- connection.php
+++ connection.php
@@ -5,6 +5,6 @@ include_once("./src/head.inc.php");
 <header>
 
     <h1>Fidelio &amp; compagny</h1>
-    <section> Vous êtes connecté, bienvenu !</section>
+    <section> Vous êtes connecté, welcome back !</section>
 
 </header>
